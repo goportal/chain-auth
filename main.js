3013a69b8c8b39f07e85ac31bcc6e7dabee0c0fd
gabriel portal <gdsoportal@restinga.ifrs.edu.br>
2017-03-08 15:27:55 -0300

    descomentando e adaptando leitura de arquivo

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ var aux1 = 0;
 
 //lendo valores de arquivo.
 //endereço
-//var leArquivo = fs.readFileSync('/Users/Portal/Desktop/docTeste.txt', 'ascii');
+var leArquivo = fs.readFileSync('/Users/Portal/Desktop/docTeste.txt', 'ascii');
 
 //var arq = leArquivo;
 
@@ -47,7 +47,7 @@ var MessageType = {
 
 var getGenesisBlock = () => {
     return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
-    //return new Block(0, "0", 1465154705, arq, "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+    //return new Block(0, "0", 1465154705, leArquivo, "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
 //colocando genesis na blockchain
@@ -79,8 +79,8 @@ var initHttpServer = () => {
     //});
 
     app.post('/mineBlock', (req, res) => {
-        var newBlock = generateNextBlock(req.body.data);
-        //var newBlock = generateNextBlock(leArquivo);
+        //var newBlock = generateNextBlock(req.body.data);
+        var newBlock = generateNextBlock(leArquivo);
         addBlock(newBlock);
         broadcast(responseLatestMsg());
         console.log('bloco adicionado:\nindice: ' + newBlock.index +'\nhash anterior: '+newBlock.previousHash+'\nTimestamp: '+newBlock.timestamp+'\ndata: '+newBlock.data+'\nhash: '+newBlock.hash+'\n');
